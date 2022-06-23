e1946ebc5b24c82d210a8c815eb7c884ba0b414a
Pawel <p.buczynski@outlook.com>
2022-06-23 19:09:00 +0200

    View.php: conversionOptions array added, isCheckboxChecked function modified; home.php: adjusted to View.php changes;

--- templates/pages/home.php
+++ templates/pages/home.php
@@ -16,14 +16,14 @@
                 <h6 class="card-title mb-4">Opcje konwersji</h5>
                     <div class="input-group align-items-start flex-nowrap py-2">
                         <div class="form-check form-switch">
-                            <input class="form-check-input option-chbox" type="checkbox" name="copy-photos" id="copy-photos" role="switch" <?php echo $this->copyPhotos; ?>>
+                            <input class="form-check-input option-chbox" type="checkbox" name="copy-photos" id="copy-photos" role="switch" <?php echo $this->conversionOptions['copy-photos']; ?>>
                             <label class="form-check-label" for="copy-photos">Kopiuj linki do zdjęć</label>
                         </div>
                         <a tabindex="0" class="d-block ms-2" data-bs-toggle="popover" data-bs-trigger="focus" data-bs-placement="top" data-bs-content="Po wczytaniu danych PrestaShop zaimportuje zdjęcia, zapisze je w miejscu swojej instalacji i dostosuje zgodnie z wprowadzonymi ustawieniami. Pliki ze zdjęciami znajdować się będą na serwerze, gdzie zainstalowany jest PrestaShop."><?php echo $this->helpIcon; ?></a>
                     </div>
                     <div class="input-group align-items-start flex-nowrap py-2">
                         <div class="form-check form-switch">
-                            <input class="form-check-input option-chbox" type="checkbox" name="copy-pictures" id="copy-pictures" role="switch" <?php echo $this->copyPictures; ?> data-bs-toggle="collapse" data-bs-target="#shop-address" aria-expanded="false" aria-controls="shop-address">
+                            <input class="form-check-input option-chbox" type="checkbox" name="copy-pictures" id="copy-pictures" role="switch" <?php echo $this->conversionOptions['copy-pictures']; ?> data-bs-toggle="collapse" data-bs-target="#shop-address" aria-expanded="false" aria-controls="shop-address">
                             <label class="form-check-label" for="copy-pictures">Kopiuj linki do grafik w opisach produktów</label>
                         </div>
                         <a tabindex="0" class="d-block ms-2" data-bs-toggle="popover" data-bs-trigger="focus" data-bs-placement="top" data-bs-content="Po wczytaniu danych PrestaShop skopiuje linki do grafik i umieści je w opisach produktów. Należy pamiętać o tym, że chociaż PrestaShop będzie wyświetlał grafiki w opisach produktów to pliki graficzne fizycznie znajdować się będą nadal na serwerze Shopera, z którego kopiowane są dane. W przypadku usunięcia ich z Shopera grafiki przestaną być widoczne również w PrestaShop. Aby pliki z grafikami znalazły się na serwerze, gdzie zainstalowany jest PrestaShop należy je tam skopiować i wprowadzić odpowiednie linki do opisów produktów."><?php echo $this->helpIcon; ?></a>
@@ -33,21 +33,21 @@
                     </div>
                     <div class="input-group align-items-start flex-nowrap py-2">
                         <div class="form-check form-switch">
-                            <input class="form-check-input option-chbox" type="checkbox" name="copy-styles" id="copy-styles" role="switch" <?php echo $this->copyStyles; ?>>
+                            <input class="form-check-input option-chbox" type="checkbox" name="copy-styles" id="copy-styles" role="switch" <?php echo $this->conversionOptions['copy-styles']; ?>>
                             <label class="form-check-label" for="copy-styles">Kopiuj style</label>
                         </div>
                         <a tabindex="0" class="d-block ms-2" data-bs-toggle="popover" data-bs-trigger="focus" data-bs-placement="top" data-bs-content="Po zaznaczeniu tej opcji opisy zostaną skopiowane do PrestaShop razem z ustawieniami styli  liniowych w html, takimi jak kolory, wielkości i kroje czcionek, pogrubienia, kursywy. Należy pamiętać o tym, że skopiowane zostaną jedynie style liniowe znajdujące się w opisach produktów w Shoperze."><?php echo $this->helpIcon; ?></a>
                     </div>
                     <div class="input-group align-items-start flex-nowrap py-2">
                         <div class="form-check form-switch">
-                            <input class="form-check-input option-chbox" type="checkbox" name="copy-bold" id="copy-bold" role="switch" <?php echo $this->copyBold; ?>>
+                            <input class="form-check-input option-chbox" type="checkbox" name="copy-bold" id="copy-bold" role="switch" <?php echo $this->conversionOptions['copy-bold']; ?>>
                             <label class="form-check-label" for="copy-bold">Kopiuj tylko pogrubienia</label>
                         </div>
                         <a tabindex="0" class="d-block ms-2" data-bs-toggle="popover" data-bs-trigger="focus" data-bs-placement="top" data-bs-content="Po zaznaczeniu tej opcji opisy zostaną skopiowane do PrestaShop razem z ustawieniami styli  liniowych w html, które dotyczą pogrubienia czcionek tj. skopiowane zostaną tagi <strong> i <b>. Należy pamiętać o tym, że skopiowane zostaną jedynie style liniowe znajdujące się w opisach produktów w Shoperze."><?php echo $this->helpIcon; ?></a>
                     </div>
                     <div class="input-group align-items-start flex-nowrap py-2">
                         <div class="form-check form-switch">
-                            <input class="form-check-input option-chbox" type="checkbox" name="deactivate-product" id="deactivate-product" role="switch" <?php echo $this->deactivateProduct; ?>>
+                            <input class="form-check-input option-chbox" type="checkbox" name="deactivate-product" id="deactivate-product" role="switch" <?php echo $this->conversionOptions['deactivate-product']; ?>>
                             <label class="form-check-label" for="deactivate-product">Ustaw produkty jako nieaktywne</label>
                         </div>
                         <a tabindex="0" class="d-block ms-2" data-bs-toggle="popover" data-bs-trigger="focus" data-bs-placement="top" data-bs-content="Włączenie tej opcji spowoduje, że wszystkie konwertowane produkty zostaną ustawione jako nieaktywne. Oznacza to, że po wczytaniu do PrestaShop nie będą one wyświetlane dopóki nie zostaną aktywowane. Jeżeli opcja ta pozostanie wyłączona aktywność produktów nie zostanie zmieniona w stosunku do ustawień w sklepie Shoper."><?php echo $this->helpIcon; ?></a>

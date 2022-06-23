3100e2a9b4d455e9ffe30dda77aec73c41950e79
Pawel <p.buczynski@outlook.com>
2022-06-23 15:29:16 +0200

    layout - meta description added

--- templates/layout.php
+++ templates/layout.php
@@ -3,6 +3,7 @@
 
 <head>
     <meta charset="UTF-8">
+    <meta name="description" content="Konwerter danych Shoper-PrestaShop. Przygotuj plik CSV z danymi produktów do wczytania do PrestaShop." />
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Konwerter danych</title>
